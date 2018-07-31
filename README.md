62f91717ef835202d9bcc96145b9ec06dbcd963e
Vedad Hadzic <vedad.hadzic@student.tugraz.at>
2018-07-31 10:55:09 +0200

    Added decryption support

---
+++ README.md
@@ -0,0 +1,23 @@
+# The Configurable LED Block Cipher
+
+## LED Specification
+- https://sites.google.com/site/ledblockcipher/design
+- http://www.springerlink.com/content/630873l12h23484p
+
+## Purpose
+This implementation is meant for security research purposes only, and should
+not be used in production. It supports both encryption and decryption.
+
+## Usage
+
+64-bit key:
+./led-64 -e -x "01234567 89abcdef" -k "01234567 89abcdef"
+// ciphertext: fdd6fb98 45f81456
+./led-64 -d -x "fdd6fb98 45f81456" -k "01234567 89abcdef"
+// plaintext: 01234567 89abcdef
+
+128-bit key:
+./led-128 -e -x "01234567 89abcdef" -k "01234567 89abcdef 01234567 89abcdef"
+// ciphertext: 3131c231 205c3664
+./led-128 -d -x "3131c231 205c3664" -k "01234567 89abcdef 01234567 89abcdef"
+// plaintext: 01234567 89abcdef
--- led.c
+++ led.c
@@ -4,7 +4,6 @@
 /* encrypt */
 void encrypt(STATE* s, KEY* k)
 {
-  
   int r;
   
   char kf = 0; /* key flag: use k_1 or k_2 */
@@ -12,10 +11,7 @@ void encrypt(STATE* s, KEY* k)
   for (r = 0; r < RN; r++)
   {
     if ((r % 4) == 0)
-    {
-      addKey(s, k, kf);
-      kf ^= 1;
-    }
+      addKey(s, k, kf), kf ^= 1;
     
     addConstants(s, r);
     subCells(s);
@@ -25,13 +21,32 @@ void encrypt(STATE* s, KEY* k)
   addKey(s, k, 0);
 }
 
+/* decrypt */
+void decrypt(STATE* s, KEY* k)
+{
+  int r;
+  char kf = (RN / 4) % 2; /* key flag: use k_1 or k_2 */
+  
+  addKey(s, k, 0);
+  for (r = RN - 1; r >= 0; r--)
+  {
+    invMixColumnsSerial(s);
+    invShiftRows(s);
+    invSubCells(s);
+    addConstants(s, r);
+    
+    if ((r % 4) == 0)
+      addKey(s, k, kf), kf ^= 1;
+  }
+}
+
 /* addKey */
 void addKey(STATE* s, KEY* k, char flag)
 {
   if (flag == 1 && KEY_SIZE == 128)
-    s->b[0] ^= k->w[2],  s->b[1] ^= k->w[3];
+    s->b[0] ^= k->w[2], s->b[1] ^= k->w[3];
   else
-    s->b[0] ^= k->w[0],  s->b[1] ^= k->w[1];
+    s->b[0] ^= k->w[0], s->b[1] ^= k->w[1];
 }
 
 /* addConstants */
@@ -56,6 +71,21 @@ void subCells(STATE* s)
   s->b[1] = x[1];
 }
 
+/* invSubCells */
+void invSubCells(STATE* s)
+{
+  int i;
+  uint32_t x[2] = {0, 0};
+  /* apply InvSBox */
+  for (i = 0; i < 8; i++)
+  {
+    x[0] ^= InvSBox[(s->b[0] >> (28 - 4 * i)) & 0xf] << (28 - 4 * i);
+    x[1] ^= InvSBox[(s->b[1] >> (28 - 4 * i)) & 0xf] << (28 - 4 * i);
+  }
+  s->b[0] = x[0];
+  s->b[1] = x[1];
+}
+
 /* shiftRows */
 void shiftRows(STATE* s)
 {
@@ -63,7 +93,14 @@ void shiftRows(STATE* s)
   s->b[1] = ((ROL16((s->b[1] >> 16) & 0xffff, 8) & 0xffff) << 16) ^ (ROL16((s->b[1] >> 0) & 0xffff, 12) & 0xffff);
 }
 
-/* mix columns */
+/* invShiftRows */
+void invShiftRows(STATE* s)
+{
+  s->b[0] = ((ROR16((s->b[0] >> 16) & 0xffff, 0) & 0xffff) << 16) ^ (ROR16((s->b[0] >> 0) & 0xffff, 4) & 0xffff);
+  s->b[1] = ((ROR16((s->b[1] >> 16) & 0xffff, 8) & 0xffff) << 16) ^ (ROR16((s->b[1] >> 0) & 0xffff, 12) & 0xffff);
+}
+
+/* mixColumnsSerial */
 void mixColumnsSerial(STATE* s)
 {
   int i;
@@ -89,6 +126,36 @@ void mixColumnsSerial(STATE* s)
   s->b[1] = x[1];
 }
 
+/* invMixColumnsSerial */
+void invMixColumnsSerial(STATE* s)
+{
+  int i;
+  uint32_t x[2] = {0, 0};
+  uint8_t t[4] = {0, 0, 0, 0};
+  
+  /* iterate over columns */
+  for (i = 0; i < 4; i++)
+  {
+    /* extract column */
+    t[0] = (uint8_t) ((s->b[0] >> (28 - 4 * i)) & 0xf);
+    t[1] = (uint8_t) ((s->b[0] >> (12 - 4 * i)) & 0xf);
+    t[2] = (uint8_t) ((s->b[1] >> (28 - 4 * i)) & 0xf);
+    t[3] = (uint8_t) ((s->b[1] >> (12 - 4 * i)) & 0xf);
+    
+    /* multiply matrix and column */
+    x[0] ^= (gm(t[0], InvMDS[0][0]) ^ gm(t[1], InvMDS[0][1]) ^ gm(t[2], InvMDS[0][2]) ^ gm(t[3], InvMDS[0][3]))
+    << (28 - 4 * i);
+    x[0] ^= (gm(t[0], InvMDS[1][0]) ^ gm(t[1], InvMDS[1][1]) ^ gm(t[2], InvMDS[1][2]) ^ gm(t[3], InvMDS[1][3]))
+    << (12 - 4 * i);
+    x[1] ^= (gm(t[0], InvMDS[2][0]) ^ gm(t[1], InvMDS[2][1]) ^ gm(t[2], InvMDS[2][2]) ^ gm(t[3], InvMDS[2][3]))
+    << (28 - 4 * i);
+    x[1] ^= (gm(t[0], InvMDS[3][0]) ^ gm(t[1], InvMDS[3][1]) ^ gm(t[2], InvMDS[3][2]) ^ gm(t[3], InvMDS[3][3]))
+    << (12 - 4 * i);
+  }
+  s->b[0] = x[0];
+  s->b[1] = x[1];
+}
+
 /* galois multiplication in GF(16) */
 uint8_t gm(uint8_t a, uint8_t b)
 {
@@ -105,16 +172,16 @@ uint8_t gm(uint8_t a, uint8_t b)
   return g;
 }
 
-
 int main(int argc, char* argv[])
 {
-  
   STATE s;
   KEY k;
   
+  enum Mode m = NONE;
+  
   int i = 0;
   
-  while ((i = getopt(argc, argv, "k:p:")) >= 0)
+  while ((i = getopt(argc, argv, "k:x:ed")) >= 0)
   {
     switch (i)
     {
@@ -137,7 +204,7 @@ int main(int argc, char* argv[])
           return EXIT_FAILURE;
         }
         break;
-      case 'p':
+      case 'x':
         if (strlen(optarg) == 17)
         {
           sscanf(strtok(optarg, " "), "%x", &s.b[0]);
@@ -149,11 +216,39 @@ int main(int argc, char* argv[])
           return EXIT_FAILURE;
         }
         break;
+      case 'e':
+        if(m == NONE)
+          m = ENCRYPT;
+        else
+        {
+          printf("Error! More than one mode selected.\n");
+          return EXIT_FAILURE;
+        }
+        break;
+      case 'd':
+        if(m == NONE)
+          m = DECRYPT;
+        else
+        {
+          printf("Error! More than one mode selected.\n");
+          return EXIT_FAILURE;
+        }
+        break;
       default:
         return EXIT_FAILURE;
     }
   }
-  encrypt(&s, &k);
+  
+  if (m == ENCRYPT)
+    encrypt(&s, &k);
+  else if (m == DECRYPT)
+    decrypt(&s, &k);
+  else
+  {
+    printf("Error! Use either encryption (-e) or decryption (-d)");
+    return EXIT_FAILURE;
+  }
+  
   printf("%08x %08x\n", s.b[0], s.b[1]);
   return EXIT_SUCCESS;
 }
